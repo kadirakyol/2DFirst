a88a69c21b9f20ccfbdba87b25f8868a485ccd9f
Kadir Akyol <102226254+kadirakyol@users.noreply.github.com>
2022-04-18 09:13:23 +0300

    TEST 3

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -7,7 +7,8 @@ public class Driver : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // Git TEST
+        
+        
     }
 
     // Update is called once per frame
