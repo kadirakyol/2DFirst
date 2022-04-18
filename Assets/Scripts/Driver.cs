d944ae5f1980ebe61bbd4b1909b116d78b4b487d
Kadir Akyol <102226254+kadirakyol@users.noreply.github.com>
2022-04-18 12:35:33 +0300

    Last Version Of The Game

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,9 @@ using UnityEngine;
 public class Driver : MonoBehaviour
 {
     [SerializeField] float steerSpeed = 0.4f;
-    [SerializeField] float moveSpeed = 0.04f;
+    [SerializeField] float moveSpeed = 12f;
+    [SerializeField] private float slowSpeed = 8f;
+    [SerializeField] private float boostSpeed = 16f;
     void Start()
     {
         
@@ -17,7 +20,20 @@ public class Driver : MonoBehaviour
         float moveAmount = Input.GetAxis("Vertical") * Time.deltaTime;
          transform.Rotate(0,0,-steerAmount * steerSpeed);
         transform.Translate(0,moveAmount * moveSpeed,0);
+    }
 
-        
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        moveSpeed = slowSpeed;
+
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Boost")
+        {
+            moveSpeed = boostSpeed;
+        }
     }
 }
